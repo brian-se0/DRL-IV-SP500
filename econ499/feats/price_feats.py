7c9a6385fa30a9f13730731b40dc43587289bf7b
brian seo <brianyseo@gmail.com>
2025-06-14 20:26:25 -0400

    Remove redundant feature wrapper scripts and update build pipeline

--- econ499/feats/price_feats.py
+++
@@ -1,4 +0,0 @@
-from iv_drl.features.build_price import main
-
-if __name__ == "__main__":
-    main() 
--- econ499/feats/surface_feats.py
+++
@@ -1,4 +0,0 @@
-from iv_drl.features.build_iv_surface import main
-
-if __name__ == "__main__":
-    main() 
